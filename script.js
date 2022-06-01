5638da034be4371a741cb6ba3f7793e0bdd75ffc
Arron Johnson <arronjohnson.uk@gmail.com>
2022-06-01 13:32:05 +0100

    Add additional slider functionality
    - Amended script.js to control resizing the grid when the slider value
    changes.
    
    - Updated style.css add styling to slider.

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-const DEFAULT_SIZE = 16;
+const DEFAULT_SIZE = 50;
 
 let currentSize = DEFAULT_SIZE;
 
+/* GRID */
 const container = document.querySelector(".grid");
-for (let i = 0; i < currentSize; i++) {
-  const row = document.createElement("div");
-  row.classList.add("row");
 
-  for (let j = 0; j < currentSize; j++) {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    cell.addEventListener("mousedown", (e) => onClick(e));
-    cell.addEventListener("mouseenter", (e) => onMouseEnter(e));
-    row.append(cell);
+function drawGrid() {
+  for (let i = 0; i < currentSize; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+
+    for (let j = 0; j < currentSize; j++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      cell.addEventListener("mousedown", (e) => colorCell(e));
+      cell.addEventListener("mouseenter", (e) => colorCell(e));
+      row.append(cell);
+    }
+    container.append(row);
   }
-  container.append(row);
 }
+drawGrid();
 
 let mouseDown = false;
 window.onmousedown = () => (mouseDown = true);
@@ -23,15 +28,28 @@ window.onmouseup = () => (mouseDown = false);
 window.ondragstart = () => false;
 window.ondrop = () => false;
 
-function onClick(e) {
-  colorCell(e.target);
+function colorCell(e) {
+  if (!mouseDown && e.type === "mouseenter") return;
+  e.target.classList.add("active");
 }
 
-function onMouseEnter(e) {
-  if (!mouseDown) return;
-  colorCell(e.target);
+function clearGrid() {
+  container.innerHTML = "";
 }
 
-function colorCell(cell) {
-  cell.classList.add("active");
+function redrawGrid() {
+  clearGrid();
+  drawGrid();
+}
+
+/* SLIDER */
+const sizeValue = document.getElementById("sizeValue");
+const sizeSlider = document.getElementById("sizeSlider");
+
+sizeSlider.oninput = (e) => updateSizeValue(e.target.value);
+sizeSlider.onchange = (e) => redrawGrid();
+
+function updateSizeValue(value) {
+  currentSize = value;
+  sizeValue.textContent = `${value} x ${value}`;
 }
